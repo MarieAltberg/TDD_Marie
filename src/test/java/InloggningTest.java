32058eaf5d8da0e4b13a6ec3ac8c1008e2c41ae0
MarieAltberg <lev247@yahoo.se>
2023-05-26 04:51:29 +0200

    InloggningTest

---
+++ src/test/java/InloggningTest.java
@@ -0,0 +1,19 @@
+public class InloggningTest {
+    private final Inloggning inloggning = new Inloggning();
+
+    @Test
+    public void userLoggInTest(){
+
+        //Given
+        TddUser tddUser = new TddUser("anna", "losen");
+        TddUser tddUser1 = new TddUser("berit", "123456");
+        TddUser tddUser2 = new TddUser("kalle", "password");
+
+        //When
+        boolean result = Inloggning.confirm(tddUser.getUsername(), tddUser.getPassword());
+
+        //Then
+        Assertions.assertTrue(result);
+
+    }
+}
