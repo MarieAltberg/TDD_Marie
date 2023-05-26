70a68eb54bfb701e929bfe1298127afd974d4696
MarieAltberg <lev247@yahoo.se>
2023-05-26 05:50:13 +0200

    Tester för 2: Autentisering

--- src/test/java/InloggningTest.java
+++ src/test/java/InloggningTest.java
@@ -1,22 +1,37 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class InloggningTest {
     private final Inloggning inloggning = new Inloggning();
 
-   @Test
-    public void userLoggInTest(){
+    @ParameterizedTest
+    @CsvSource(value = {"anna, losen, true", "berit, 123456, true", "kalle, password, true"})
 
+
+   /* @Test
+    public void userLoggInTest() {
         //Given
         TddUser tddUser = new TddUser("anna", "losen");
         TddUser tddUser1 = new TddUser("berit", "123456");
         TddUser tddUser2 = new TddUser("kalle", "password");
-
         //When
         boolean result = Inloggning.confirm(tddUser.getUsername(), tddUser.getPassword());
-
         //Then
         Assertions.assertTrue(result);
 
     }
+*/
+    public void userLoggInTest(String username, String password, boolean expected){
+        boolean result = Inloggning.confirm(username, password);
+        Assertions.assertEquals(expected, result);
+    }
+
+    public void autentiseringTest(String username, String password, boolean expected){
+        String token = Inloggning.createSecurity(username, password);
+        boolean result = Inloggning.confirmationUser(username, password, token);
+        Assertions.assertEquals(expected, result);
+    }
+
 }
