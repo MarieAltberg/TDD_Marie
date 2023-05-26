d34934240f87a1dbb8c405d3fc2ede84465495e0
MarieAltberg <lev247@yahoo.se>
2023-05-26 08:27:08 +0200

    mer 2: Authentisering

--- src/main/java/Inloggning.java
+++ src/main/java/Inloggning.java
@@ -1,4 +1,15 @@
-public class Inloggning {
+import java.security.Key;
+import java.util.List;
+import java.util.UUID;
+
+public class Inloggning{
+    TddUser tddUser = new TddUser("Anna", "losen");
+    TddUser tddUser2 = new TddUser("berit", "123456");
+    TddUser tddUser3 = new TddUser("kalle", "password");
+
+    List<TddUser> AllTddUser = List.of(tddUser,tddUser2,tddUser3);
+
+
     private static String username;
     private static String password;
 
@@ -7,5 +18,18 @@ public class Inloggning {
         Inloggning.password = password;
         return true;
     }
+
+
+
+
+
+    public static String createSecurity(String username, String password) {
+        return null;
+    }
+
+    public static boolean confirmationUser(String username, String password, String token) {
+        return true;
+    }
+
 
 }
--- src/test/java/InloggningTest.java
+++ src/test/java/InloggningTest.java
@@ -3,26 +3,58 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+
+import java.util.Base64;
+
 public class InloggningTest {
     private final Inloggning inloggning = new Inloggning();
 
     @ParameterizedTest
     @CsvSource(value = {"anna, losen, true", "berit, 123456, true", "kalle, password, true"})
+    //@CsvSource(value = "anna, losen, true", "berit, 123456, true", "kalle, password, true")
 
 
-   /* @Test
-    public void userLoggInTest() {
-        //Given
-        TddUser tddUser = new TddUser("anna", "losen");
-        TddUser tddUser1 = new TddUser("berit", "123456");
-        TddUser tddUser2 = new TddUser("kalle", "password");
-        //When
-        boolean result = Inloggning.confirm(tddUser.getUsername(), tddUser.getPassword());
-        //Then
-        Assertions.assertTrue(result);
+    @Test
+    public void base64Password(){
+        String original = "losen";
+        byte[] originalAsBytes = original.getBytes();
+        byte[] base64bytes = Base64.getEncoder().encode(originalAsBytes);
+        String base64String = new String(base64bytes);
+
+
+
+      //  byte[] backAsBases64bytes = base64String1.getBytes();
+      //  byte[] backAsBytes = Base64.getDecoder().decode(backAsBases64bytes);
+      //  String backAsString = new String(backAsBytes);
+
+       Assertions.assertEquals(base64bytes, base64bytes);
+        Assertions.assertEquals(originalAsBytes, originalAsBytes);
+
+
 
     }
-*/
+
+
+
+
+
+
+
+
+
+    /* @Test
+     public void userLoggInTest() {
+         //Given
+         TddUser tddUser = new TddUser("anna", "losen");
+         TddUser tddUser1 = new TddUser("berit", "123456");
+         TddUser tddUser2 = new TddUser("kalle", "password");
+         //When
+         boolean result = Inloggning.confirm(tddUser.getUsername(), tddUser.getPassword());
+         //Then
+         Assertions.assertTrue(result);
+
+     }
+ */
     public void userLoggInTest(String username, String password, boolean expected){
         boolean result = Inloggning.confirm(username, password);
         Assertions.assertEquals(expected, result);
